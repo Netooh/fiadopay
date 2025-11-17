1914ce365583aa62be30dce6e78c683e42074327
Netooh <celsoneto95@hotmail.com>
2025-11-17 10:37:00 -0300

    feat(events): adicionar handler UserRegisteredHandler com @EventHandler

--- src/main/java/edu/ucsal/fiadopay/core/events/UserRegisteredHandler.java
+++ src/main/java/edu/ucsal/fiadopay/core/events/UserRegisteredHandler.java
@@ -1,4 +1,11 @@
 package edu.ucsal.fiadopay.core.events;
 
+import edu.ucsal.fiadopay.annotations.EventHandler;
+
+@EventHandler(event = "USER_REGISTERED")
 public class UserRegisteredHandler {
+
+    public void handle(String data) {
+        System.out.println("Evento USER_REGISTERED recebido com dados: " + data);
+    }
 }
